894aada61637ec91240a0ce42ebf339fec18837b
efdssdf <efdssdf@gmail.com>
2018-10-25 17:41:06 +0800

    'fix'

--- model/tongji.js
+++ model/tongji.js
@@ -6,8 +6,6 @@ var db = mongoose.createConnection(connect_url);
 var TongjiSchema = new Schema({
 	url:String,
     out_url:String,
-    qudao:String, //渠道名
-    name:String,//平台+服务号
     pv:Number,  //pv数量
     uv:Number, //uv数量
     ip_count:Number,  //ip数量
@@ -21,7 +19,7 @@ var TongjiSchema = new Schema({
     today_money:Number, //今日充值
     today_cost:Number, //今日成本
     today_back:String, //动态回本率
-    linktime: String,
+    name:String,//平台+服务号
     createtime:Number //创建时间
 });
 
